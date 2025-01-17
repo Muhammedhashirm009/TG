4eb68c5ca269ea1dee7f30847e49fa09558b953e
Muhammedhashirm009 <87589712+Muhammedhashirm009@users.noreply.github.com>
2025-01-17 23:42:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,6 @@ if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
   git clone https://github.com/Muhammedhashirm009/File-Forward-Bot.git 
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO
 fi
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
 python3 bot.py
